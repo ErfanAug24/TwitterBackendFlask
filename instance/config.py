04df76915811d812be53cf34d307757f7031707e
erfan <eshirkhanei261384@gmail.com>
2025-02-15 14:11:41 +0330

    adding TestingConfig

--- instance/config.py
+++ instance/config.py
@@ -1,18 +1,26 @@
 import os
 import secrets
 from datetime import timedelta
+from twitter.settings import ROOT_PATH
 
 
 class Config(object):
+    # Flask
     SECRET_KEY = secrets.token_hex(32)
+    # Flask Jwt Extended
     JWT_SECRET_KEY = secrets.token_hex(64)
     JWT_COOKIE_SECURE = False
     JWT_COOKIE_CSRF_PROTECT = True
     JWT_TOKEN_LOCATION = ["headers", "cookies", "json", "query_string"]
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # !(override them in production)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)  # !(override them in production)
-    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.curdir), "/Statics")
-    ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif"]
+    # Flask Uploads
+    UPLOADED_PHOTOS_DEST = "uploads/images"
+    UPLOADED_FILES_DEST = "uploads/images"
+    UPLOADED_FILES_URL = "http://localhost:5001/photos"
+    UPLOADED_FILES_ALLOW = ["png", "jpg", "jpeg", "gif", "mp4", "mkv"]
+    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.curdir), "/static")
+    ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg", "gif", "mp4", "mkv"]
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -31,4 +39,4 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
-    pass
+    TESTING = True
