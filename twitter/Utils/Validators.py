8d1f71ea44a7a9313f49a4d6bacf79df8bac2315
erfan <eshirkhanei261384@gmail.com>
2025-02-15 14:14:44 +0330

    adding schema_validator

---
+++ twitter/Utils/Validators.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify
+from functools import wraps
+
+from marshmallow import ValidationError
+
+# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from twitter.Config.sqlalchemy_conf import db
+
+
+def schema_validator(schema):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            data = request.get_json(force=True)
+            try:
+                object_schema = schema(session=db.session)
+                validated_data = object_schema.load(data)
+            except ValidationError as err:
+                return jsonify({"errors": err.messages})
+
+            return func(*args, **kwargs, data=validated_data)
+
+        return wrapper
+
+    return decorator
